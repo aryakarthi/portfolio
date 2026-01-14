fd9ba5ffcd16f6a0e9ae34a7fd70ebf63f43ba32
aryakarthi <aryaperumal.m@gmail.com>
2026-01-15 00:40:43 +0530

    updated navbar breakpoints

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ import {
 } from "react-icons/hi2";
 
 // nav data
-export const navData = [
+const navData = [
   { name: "home", path: "/", icon: <HiHome /> },
   { name: "about", path: "/about", icon: <HiUser /> },
   { name: "projects", path: "/projects", icon: <HiRectangleGroup /> },
@@ -23,8 +23,8 @@ export const navData = [
 const Nav = () => {
   const location = useLocation();
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-10 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+    <nav className="flex flex-col items-center lg:justify-center gap-y-4 fixed h-max bottom-0 mt-auto lg:right-[2%] z-50 top-0 w-full lg:w-16 lg:max-w-md lg:h-screen">
+      <div className="flex w-full lg:flex-col items-center justify-between lg:justify-center gap-y-10 px-10 h-[80px] lg:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl lg:text-xl lg:rounded-full">
         {navData.map((link, index) => {
           return (
             <Link
@@ -34,7 +34,7 @@ const Nav = () => {
                 link.path === location.pathname && "text-accent"
               } relative flex items-center group hover:text-accent`}
             >
-              <span className="absolute right-14 hidden xl:group-hover:flex bg-white text-primary p-2 rounded-[4px] text-[12px] leading-none font-semibold capitalize">
+              <span className="absolute right-14 hidden lg:group-hover:flex bg-white text-primary p-2 rounded-[4px] text-[12px] leading-none font-semibold capitalize">
                 {link.name}
               </span>
 
